e6c208b63d92754a3bcf0f5c1c4fd8e335ad5c63
jamie gist <jamie.gist.7@gmail.com>
2024-10-19 13:51:40 -0700

    Added routes. Needs debugging.

--- little-lemon-site/src/App.js
+++ little-lemon-site/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import HomePage from './HomePage';
 import BookingForm from './BookingForm';
 import Header from './Header';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <div className="main">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/booking" element={<BookingForm />}></Route>
+      </Routes>
+    </BrowserRouter>
       <Header />
       <BookingForm />
+    </div>
     </div>
   );
 }
